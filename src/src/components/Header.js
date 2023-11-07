3a7d743e475893251031d93de43bdc391edf9f16
Elisa Smelly <e.edson@protonmail.ch>
2023-11-07 16:44:08 +0000

    login drop-box

--- src/src/components/Header.js
+++ src/src/components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import './header.css'
-import Dropdown from './dropdown';
+import Dropdown from './dropdown.js';
 import { useState } from 'react'
 
 export default function Header() {
     const [searchValue, setSearchValue] = useState();
+    const [toggle, setToggle] = useState(true);
 
     function handleKeyPress(e){
         if (e.key === 'Enter'){
@@ -20,15 +21,14 @@ export default function Header() {
         setSearchValue(event.target.value);
     }
 
-    function loginDropdown(){
-
-    }
-
     return (
         <div id="header">
             <h2 id="title">Sam and Ellie Recipes</h2>
             <input id="searchbar" type="text" placeholder='Find Recipes' onKeyDown={(event) => handleKeyPress(event)} onChangeCapture={(event) => handleSearchValue(event)}></input>
-            <button id="login" onClick={() => loginDropdown()}>Admin Login</button>
+            <button id="login" onClick={() => (setToggle(!toggle))}>Login</button>
+            <div>
+            {toggle && (<Dropdown></Dropdown>)}
+            </div>
         </div>
     )
 }
