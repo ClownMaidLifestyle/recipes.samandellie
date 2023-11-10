c98b532f19ec9265cc498fdced16b6250bada707
Elisa Smelly <e.edson@protonmail.ch>
2023-11-10 11:24:54 +0000

    footer basics

--- src/src/components/Header.js
+++ src/src/components/Header.js
@@ -5,7 +5,7 @@ import { useState } from 'react'
 
 export default function Header() {
     const [searchValue, setSearchValue] = useState();
-    const [toggle, setToggle] = useState(true);
+    const [toggle, setToggle] = useState(false);
 
     function handleKeyPress(e){
         if (e.key === 'Enter'){
