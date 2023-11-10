33ef331c6753c4df4b6160ba8ac5ff580550635b
Elisa Smelly <e.edson@protonmail.ch>
2023-11-10 11:41:07 +0000

    whitespace fix

---
+++ src/src/components/dropdown.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./dropdown.css"
+
+export default function Dropdown() {
+  return (
+    <div id="dropdownMenu">
+      <input placeholder="User ID"></input>
+      <input placeholder="Password"></input>
+    </div>
+  )
+}
